1d2cf00b92f4186e0675755fda94672bc01069ac
Nina S Quansah <ninaspioquansah@gmail.com>
2023-08-09 12:54:25 +0000

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,26 +1,86 @@
 #!/usr/bin/env python3
-"""Contains password hashing algorithm"""
-import bcrypt
-from typing import Union
+""" 0x05. Personal data
+"""
+
+import logging
+import os
+import re
+import mysql.connector
+from typing import List
 
 
-def hash_password(password: str) -> bytes:
-    """Hashes a password
-    Args:
-        password (str): password to be hashed
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ RedactingFormatter class. """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Init """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ Implementing a logger.
     """
-    if type(password) is not str:
-        return b""
-    pw: bytes = password.encode('utf-8')
-    hashed_pw: bytes = bcrypt.hashpw(pw, bcrypt.gensalt())
 
-    return hashed_pw
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
 
 
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Validates @hashed_password with the provided @password"""
-    if type(hashed_password) is not bytes or type(password) is not str:
-        return False
-    dec_pw: bytes = password.encode()
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Implement db conectivity
+    """
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
 
-    return bcrypt.checkpw(dec_pw, hashed_password)
+
+def main() -> None:
+    """ Implement a main function
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
